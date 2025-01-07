38130d3cac3ce2bb9b2129f4d7a354c589291d2e
WAcAW9 <wacaw9@gmail.com>
2025-01-07 21:03:38 +0900

    article search

--- src/Main.java
+++ src/Main.java
@@ -3,104 +3,12 @@ import java.util.*;
 public class Main {
 	public static void main(String[] args) {
 		
-		// 1. 입력 대기 -> 입력시 즉시 종료
-		// 데이터 타입 변수이름 = 객체 만들기 생성자(인자(클래스.변수))
-		/*Scanner input = new Scanner(System.in);
-		
-		System.out.println("== 프로그램 시작 ==");
-		System.out.print("명령어)");
-		
-		String cmd=input.nextLine();
-		
-		System.out.println("입력된 명령어 : " + cmd);
-		System.out.println("== 프로그램 끝 ==");*/
-		
-		
-		
-		// 2. 종료 의사 표시 -> 종료
-		
-		/*Scanner input = new Scanner(System.in);
-		// 경고 : 자원이 영원히 켜져있음 
-		
-		System.out.println("== 프로그램 시작 ==");
-		
-		while(true) {
-			System.out.print("명령어)");
-			String cmd=input.nextLine();
-			System.out.println("입력된 명령어 : " + cmd);
-			if(cmd.equals("exit")) break; 
-			
-			//why .equals()? -> 부등호(==)는 주소 비교 결과,
-			// 순수한 값만 비교하기 위해 문자열 비교는 equals 사용
-			
-			// String a = "a"; 1차원적 값을 직접 넣을 수 있는 특별한 String 자료형
-			// String b = new String("a"); 객체 형태 데이터를 넣어야 함
-			
-			// a==b : false
-			// a.equals(b) : true
-			
-			// 변수 : stack에 저장,같다면 동일 주소 사용
-			// instance : heap에 저장, 주소 별도 사용
-			
-		}
-		
-		// input 자원 끝내기
-		input.close();
-
-		System.out.println("== 프로그램 끝 ==");
-		*/
-		
-		//3. 게시글 작성과 입출력
-		/*Scanner sc = new Scanner(System.in);
-		int lastArticleId = 0;
-		
-		System.out.println("== 프로그램 시작 ==");
-		
-		while(true) {
-			System.out.print("명령어)");
-			String cmd = sc.nextLine().trim(); //앞뒤 공백 제거
-			
-			// 0) 종료 명령어
-			if(cmd.equals("exit")) break;
-			
-			// 1) 공백 입력
-			// nextLine() return 타입 검증 필요(null,"")
-			if(cmd.equals("")) { //cmd.length()==0 => 공백 확인 가능
-				System.out.println("명령어를 입력해주세요");
-				continue;
-			}
-			
-			// 2) 게시글 입력 명령어
-			if(cmd.equals("article write")) {
-				System.out.print("제목 : ");
-				sc.nextLine();
-				System.out.print("내용 : ");
-				sc.nextLine();
-				
-				System.out.println(++lastArticleId + "번 글이 생성되었습니다");
-			}
-			// 3) 게시글 출력 명령어
-			else if(cmd.equals("article list")) {
-				System.out.println("게시글이 없습니다");
-			}
-			// 4) 정의되지 않은 명령어
-			else {
-				System.out.println("존재하지 않는 명령어입니다.");
-			}
-			
-		} //while
-		
-		sc.close();
-		System.out.println("== 프로그램 종료 ==");
-		*/
-		
-		//4. 게시글 작성 저장
-		// 복수개의 데이터 처리를 위한 List
-		// 게시물 데이터 타입 생성, 번호/제목/내용 데이터 저장
+		//5. 게시글 상세보기
+		// substring, split, startWith
 		Scanner sc = new Scanner(System.in);
 		
 		int lastArticleId = 0;
-		// 게시물 List 생성
+
 		List<Article> articles = new ArrayList<>();
 		
 		System.out.println("== 프로그램 시작 ==");
@@ -120,7 +28,7 @@ public class Main {
 			
 			// 2) 게시글 입력 명령어
 			if(cmd.equals("article write")) {
-				// 내용 입력받기
+
 				System.out.print("제목 : ");
 				String title = sc.nextLine();
 				System.out.print("내용 : ");
@@ -128,10 +36,7 @@ public class Main {
 				
 				lastArticleId++;
 				
-				// 객체 생성
 				Article article = new Article(lastArticleId,title,body);
-				
-				// 리스트에 저장
 				articles.add(article);
 				
 				System.out.println(lastArticleId + "번 글이 생성되었습니다");
@@ -151,7 +56,46 @@ public class Main {
 				}
 				
 			}
-			// 4) 정의되지 않은 명령어
+			// 4) 게시물 상새보기
+			else if(cmd.startsWith("article detail ")) { // 뒤에 띄어쓰기 필수
+				// 명령어 분해
+				String[] cmdBits = cmd.split(" "); //공백 마다 자르기 -> 단어별로 배열 원소로 저장
+				
+				int id=0;
+				
+				// 예외처리
+				try {
+					id=Integer.parseInt(cmdBits[2]);
+				}catch(NumberFormatException e) {
+					System.out.println("명령어가 올바르지 않습니다.");
+					continue;
+				}
+				catch(Exception e) {
+					System.out.println("error : "+ e);
+				}
+				
+				// 게시물 찾기(순회)
+				
+				Article foundArticle = null;
+				
+				for(Article article : articles) {
+					if(id == article.id) { // 데이터 무결성 -> 타입변환 주의
+						foundArticle = article;
+						break;
+					}
+				}
+				
+				if(foundArticle ==null) {
+					System.out.println(id+"번 게시물은 존재하지 않습니다.");
+					continue;
+				}
+				
+				System.out.printf("번호: %d\n",foundArticle.id);
+				System.out.printf("제목: %s\n",foundArticle.title);
+				System.out.printf("내용: %s\n",foundArticle.body);
+				
+			}
+			// 5) 정의되지 않은 명령어
 			else {
 				System.out.println("존재하지 않는 명령어입니다.");
 			}
@@ -170,7 +114,6 @@ class Article{
 	String title;
 	String body;
 	
-	// 생성자
 	public Article(int id, String title, String body) {
 		this.id=id;
 		this.title=title;
