5ca5702e58f477ccc30e283d2241ce7051380aef
WAcAW9 <wacaw9@gmail.com>
2025-01-07 19:03:05 +0900

    article list

--- src/Main.java
+++ src/Main.java
@@ -51,10 +51,7 @@ public class Main {
 		*/
 		
 		//3. 게시글 작성과 입출력
-		// 동적 메모리 할당, switch-case문
-		
-		Scanner sc = new Scanner(System.in);
-		//String[] article = new String[][];
+		/*Scanner sc = new Scanner(System.in);
 		int lastArticleId = 0;
 		
 		System.out.println("== 프로그램 시작 ==");
@@ -95,5 +92,88 @@ public class Main {
 		
 		sc.close();
 		System.out.println("== 프로그램 종료 ==");
+		*/
+		
+		//4. 게시글 작성 저장
+		// 복수개의 데이터 처리를 위한 List
+		// 게시물 데이터 타입 생성, 번호/제목/내용 데이터 저장
+		Scanner sc = new Scanner(System.in);
+		
+		int lastArticleId = 0;
+		// 게시물 List 생성
+		List<Article> articles = new ArrayList<>();
+		
+		System.out.println("== 프로그램 시작 ==");
+		
+		while(true) {
+			System.out.print("명령어)");
+			String cmd = sc.nextLine().trim();
+			
+			// 0) 종료 명령어
+			if(cmd.equals("exit")) break;
+			
+			// 1) 공백 입력
+			if(cmd.equals("")) {
+				System.out.println("명령어를 입력해주세요");
+				continue;
+			}
+			
+			// 2) 게시글 입력 명령어
+			if(cmd.equals("article write")) {
+				// 내용 입력받기
+				System.out.print("제목 : ");
+				String title = sc.nextLine();
+				System.out.print("내용 : ");
+				String body = sc.nextLine();
+				
+				lastArticleId++;
+				
+				// 객체 생성
+				Article article = new Article(lastArticleId,title,body);
+				
+				// 리스트에 저장
+				articles.add(article);
+				
+				System.out.println(lastArticleId + "번 글이 생성되었습니다");
+			}
+			// 3) 게시글 출력 명령어
+			else if(cmd.equals("article list")) {
+				// 게시물이 존재하지 않는 경우
+				if(articles.size()==0) {
+					System.out.println("게시글이 없습니다");		
+					continue;
+				}
+				// 게시물이 존재하는 경우
+				System.out.println("번호 | 제목");
+				for(int i=articles.size()-1;i>=0;i--) {
+					System.out.printf("%d   |   %s\n",articles.get(i).id,articles.get(i).title);
+				
+				}
+				
+			}
+			// 4) 정의되지 않은 명령어
+			else {
+				System.out.println("존재하지 않는 명령어입니다.");
+			}
+			
+		} //while
+		
+		sc.close();
+		System.out.println("== 프로그램 종료 ==");
+		
+		
+	}
+}
+
+class Article{
+	int id;
+	String title;
+	String body;
+	
+	// 생성자
+	public Article(int id, String title, String body) {
+		this.id=id;
+		this.title=title;
+		this.body=body;
 	}
 }
